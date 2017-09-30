5b1023b2f97b65f0a2e8b2f010cba69bddd2e403
Himanshu Mittal <himanshu.mittal224@gmail.com>
2017-10-01 00:49:14 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,8 @@
 
 Python code of Biogeography-Based Optimization (BBO)
 Coded by: Raju Pal (emailid: raju3131.pal@gmail.com) and Himanshu Mittal (emailid: himanshu.mittal224@gmail.com)
-The code template used is similar to code given at link: https://github.com/7ossam81/EvoloPy
+The code template used is similar to code given at link: https://github.com/himanshuRepo/CKGSA-in-Python 
+ and matlab version of the BBO at link: http://embeddedlab.csuohio.edu/BBO/software/
 
 Reference: D. Simon, Biogeography-Based Optimization, IEEE Transactions on Evolutionary Computation, in print (2008).
 @author: Dan Simon (http://embeddedlab.csuohio.edu/BBO/software/)
